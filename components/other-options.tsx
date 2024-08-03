bd3cc893fda6e42c746fdfb0a67434a93ecefdcf
saigenix <sainathmahindrakar@gmail.com>
2024-08-04 03:05:28 +0530

    [add]  stars and rank by stars

---
+++ components/other-options.tsx
@@ -0,0 +1,188 @@
+import React from "react";
+import {
+  Container,
+  Box,
+  ButtonGroup,
+  Button,
+  Icon,
+  Heading,
+  Text,
+  IconButton,
+  VStack,
+  Flex,
+  Spinner,
+  Center,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  useClipboard,
+  useToast,
+} from "@chakra-ui/react";
+import { SlActionRedo, SlHeart } from "react-icons/sl";
+import { BsBookmarkPlus, BsHeartFill, BsHeart } from "react-icons/bs";
+import { updateStars, saveTool } from "utils/firestore";
+function OtherOptions({ toolId, stars }) {
+  const [likes, setLikes] = React.useState(stars);
+  const [saved, setSaved] = React.useState(false);
+  const [shareToolId, setShareToolId] = React.useState<string | null>(null);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [link, setLink] = React.useState<string>("");
+  const { hasCopied, onCopy } = useClipboard(link);
+  const toast = useToast();
+  React.useEffect(() => {
+    if (typeof window !== "undefined") {
+      setLink(window.location.href);
+    }
+  }, []);
+
+  const handleLike = async (toolId: string) => {
+    if (likes > stars) {
+      setLikes((prevLikes) => prevLikes - 1);
+      await updateStars(toolId, -1);
+      return;
+    }
+    setLikes((prevLikes) => prevLikes + 1);
+    await updateStars(toolId, 1);
+  };
+
+  const handleShare = (toolId: string) => {
+    setShareToolId(toolId);
+    if (navigator.share) {
+      navigator
+        .share({
+          title: "Gemini ToolKit",
+          url: `${window.location.origin}/tool?toolID=${toolId}`,
+        })
+        .catch(console.error);
+    } else {
+      onOpen();
+    }
+  };
+
+  const handleSave = async (toolId: string) => {
+    setSaved((prevSaved) => !prevSaved);
+
+    if (!saved) {
+      try {
+        // await saveTool(toolId);
+        toast({
+          title: "Tool saved successfully.",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+          position: "top-right",
+        });
+      } catch (error) {
+        console.error("Error saving tool:", error);
+        toast({
+          title: "Failed to save tool.",
+          description: "An error occurred while saving the tool.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+          position: "top-right",
+        });
+      }
+    }
+  };
+  return (
+    <>
+      <Flex justifyContent="space-between" alignItems="center" mt={10}>
+        <Flex alignItems="center" mr="1.2rem">
+          <Icon
+            as={likes > stars ? BsHeartFill : BsHeart}
+            boxSize="1.2rem"
+            cursor="pointer"
+            marginRight="0.5rem"
+            color={likes ? "red" : ""}
+            onClick={() => handleLike(toolId)}
+          />
+          <Text fontSize="sm">{likes}</Text>
+        </Flex>
+        <Icon
+          as={SlActionRedo}
+          boxSize="1.2rem"
+          cursor="pointer"
+          marginRight="1.2rem"
+          onClick={() => handleShare(toolId)}
+        />
+        <Icon
+          as={BsBookmarkPlus}
+          boxSize="1.2rem"
+          cursor="pointer"
+          marginRight="1.2rem"
+          color={saved ? "" : ""}
+          onClick={() => handleSave(toolId)}
+        />
+      </Flex>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Share this Tool</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <ButtonGroup spacing={4}>
+              <Button
+                colorScheme="whatsapp"
+                onClick={() =>
+                  window.open(
+                    `https://api.whatsapp.com/send?text=${encodeURIComponent(
+                      `${window.location.origin}/tool?toolID=${shareToolId}`
+                    )}`,
+                    "_blank"
+                  )
+                }
+              >
+                WhatsApp
+              </Button>
+              <Button
+                colorScheme="linkedin"
+                onClick={() =>
+                  window.open(
+                    `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(
+                      `${window.location.origin}/tool?toolID=${shareToolId}`
+                    )}`,
+                    "_blank"
+                  )
+                }
+              >
+                LinkedIn
+              </Button>
+              <Button
+                colorScheme="red"
+                onClick={() =>
+                  window.open(
+                    `https://www.instagram.com/?url=${encodeURIComponent(
+                      `${window.location.origin}/tool?toolID=${shareToolId}`
+                    )}`,
+                    "_blank"
+                  )
+                }
+              >
+                Instagram
+              </Button>
+              <Button onClick={onCopy}>
+                {hasCopied ? "Link Copied" : "Copy Link"}
+              </Button>
+            </ButtonGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+export default OtherOptions;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,7 @@ import * as React from "react";
 import type { NextPage } from "next";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
-import { SlActionRedo, SlHeart } from "react-icons/sl";
-import { BsBookmarkPlus, BsHeartFill, BsHeart } from "react-icons/bs";
+import OtherOptions from "components/other-options";
 import { css, keyframes } from "@emotion/react";
 import geminiLogo from "../public/static/images/gemini.png";
 import { useRouter } from "next/router";
@@ -39,7 +38,7 @@ import { SEO } from "components/seo/seo";
 import { BackgroundGradient } from "components/gradients/background-gradient";
 import { ButtonLink } from "components/button-link/button-link";
 import { Highlights, HighlightsItem } from "components/highlights";
-import { GetAllData, saveTool } from "utils/firestore";
+import { GetAllData, saveTool,updateStars } from "utils/firestore";
 import { requestPermission } from "utils/firebase-messaging";
 import { MdVerified } from "react-icons/md";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -177,71 +176,6 @@ const ExploreTools: React.FC = () => {
 };
 
 const HighlightsSection = ({ tools }: any) => {
-  const [likes, setLikes] = React.useState<{ [key: string]: number }>({});
-  const [saved, setSaved] = React.useState<{ [key: string]: boolean }>({});
-  const [shareToolId, setShareToolId] = React.useState<string | null>(null);
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [link, setLink] = React.useState<string>("");
-  const { hasCopied, onCopy } = useClipboard(link);
-  const toast = useToast();
-
-  React.useEffect(() => {
-    if (typeof window !== "undefined") {
-      setLink(window.location.href);
-    }
-  }, []);
-
-  const handleLike = (toolId: string) => {
-    setLikes((prevLikes) => ({
-      ...prevLikes,
-      [toolId]: (prevLikes[toolId] || 0) + 1,
-    }));
-  };
-
-  const handleShare = (toolId: string) => {
-    setShareToolId(toolId);
-    if (navigator.share) {
-      navigator
-        .share({
-          title: "Gemini ToolKit",
-          url: `${window.location.origin}/tool?toolID=${toolId}`,
-        })
-        .catch(console.error);
-    } else {
-      onOpen();
-    }
-  };
-
-  const handleSave = async (toolId: string) => {
-    setSaved((prevSaved) => ({
-      ...prevSaved,
-      [toolId]: !prevSaved[toolId],
-    }));
-
-    if (!saved[toolId]) {
-      try {
-        await saveTool(toolId);
-        toast({
-          title: "Tool saved successfully.",
-          status: "success",
-          duration: 3000,
-          isClosable: true,
-          position: "top-right",
-        });
-      } catch (error) {
-        console.error("Error saving tool:", error);
-        toast({
-          title: "Failed to save tool.",
-          description: "An error occurred while saving the tool.",
-          status: "error",
-          duration: 3000,
-          isClosable: true,
-          position: "top-right",
-        });
-      }
-    }
-  };
-
   const circularMotion = keyframes`
   0% {
     transform: rotate(0deg);
@@ -308,95 +242,11 @@ const HighlightsSection = ({ tools }: any) => {
               </ButtonLink>
             </ButtonGroup>
           </Flex>
-          <Flex justifyContent="space-between" alignItems="center" mt={10}>
-            <Flex alignItems="center" mr="1.2rem">
-              <Icon
-                as={likes[highlight.id] ? BsHeartFill : BsHeart}
-                boxSize="1.2rem"
-                cursor="pointer"
-                marginRight="0.5rem"
-                color={likes[highlight.id] ? "red" : ""}
-                onClick={() => handleLike(highlight.id)}
-              />
-              <Text fontSize="sm">{likes[highlight.id] || 0}</Text>
-            </Flex>
-            <Icon
-              as={SlActionRedo}
-              boxSize="1.2rem"
-              cursor="pointer"
-              marginRight="1.2rem"
-              onClick={() => handleShare(highlight.id)}
-            />
-            <Icon
-              as={BsBookmarkPlus}
-              boxSize="1.2rem"
-              cursor="pointer"
-              marginRight="1.2rem"
-              color={saved[highlight.id] ? "" : ""}
-              onClick={() => handleSave(highlight.id)}
-            />
-          </Flex>
+          <OtherOptions toolId={highlight.id} stars={highlight.stars} />
         </HighlightsItem>
       ))}
 
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Share this Tool</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <ButtonGroup spacing={4}>
-              <Button
-                colorScheme="whatsapp"
-                onClick={() =>
-                  window.open(
-                    `https://api.whatsapp.com/send?text=${encodeURIComponent(
-                      `${window.location.origin}/tool?toolID=${shareToolId}`
-                    )}`,
-                    "_blank"
-                  )
-                }
-              >
-                WhatsApp
-              </Button>
-              <Button
-                colorScheme="linkedin"
-                onClick={() =>
-                  window.open(
-                    `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(
-                      `${window.location.origin}/tool?toolID=${shareToolId}`
-                    )}`,
-                    "_blank"
-                  )
-                }
-              >
-                LinkedIn
-              </Button>
-              <Button
-                colorScheme="red"
-                onClick={() =>
-                  window.open(
-                    `https://www.instagram.com/?url=${encodeURIComponent(
-                      `${window.location.origin}/tool?toolID=${shareToolId}`
-                    )}`,
-                    "_blank"
-                  )
-                }
-              >
-                Instagram
-              </Button>
-              <Button onClick={onCopy}>
-                {hasCopied ? "Link Copied" : "Copy Link"}
-              </Button>
-            </ButtonGroup>
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+
     </Highlights>
   );
 };
